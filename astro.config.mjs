7c4227fd9ad48616bb313b8cc97012b8956c4653
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-08-14 20:22:14 +0800

    Add TailwindCSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 });
