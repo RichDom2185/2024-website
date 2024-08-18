bd5ff98eac4b79410caa9fb7e712c5cfb81a72c2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-08-18 18:48:33 +0800

    Add remark plugins and update syntax highlighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import { remarkTruncateLinks } from 'remark-truncate-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react(), tailwind()],
+  markdown: {
+    syntaxHighlight: 'prism',
+    remarkPlugins: [remarkMath, remarkTruncateLinks],
+  },
 });
