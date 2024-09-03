bb8e4f81bc7c416244313628f55c1a4bdb4ec0ad
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-03 22:46:50 +0800

    Migrate sidebar and subcomponents to Astro

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,7 +1,13 @@
+const SOCIAL_EMAIL_URL: string = import.meta.env.SOCIAL_EMAIL_URL ?? '';
+const SOCIAL_GITHUB_URL: string = import.meta.env.SOCIAL_GITHUB_URL ?? '';
+const SOCIAL_GITLAB_URL: string = import.meta.env.SOCIAL_GITLAB_URL ?? '';
 const SOCIAL_LINKEDIN_URL: string = import.meta.env.SOCIAL_LINKEDIN_URL ?? '';
 
 export const Constants = {
   social: {
+    EMAIL_URL: SOCIAL_EMAIL_URL,
+    GITHUB_URL: SOCIAL_GITHUB_URL,
+    GITLAB_URL: SOCIAL_GITLAB_URL,
     LINKEDIN_URL: SOCIAL_LINKEDIN_URL,
   },
 } as const;
