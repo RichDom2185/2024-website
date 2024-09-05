4336a06266d4a91a8372f4fbe32b76b146760a05
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-06 00:19:24 +0800

    Allow forcing Tailwind component theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,15 @@ export default {
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,json}',
     './astro.config.mjs',
   ],
+  // Allow both automatic and manual dark mode. See:
+  // https://tailwindcss.com/docs/dark-mode#using-multiple-selectors
+  darkMode: [
+    'variant',
+    [
+      '@media (prefers-color-scheme: dark) { &:not(.light *) }',
+      '&:is(.dark *)',
+    ],
+  ],
   theme: {
     extend: {
       fontFamily: {
