154faa808afe1a1f2b6e8a423a4293cadcee84d4
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-05 18:42:15 +0800

    Configure fonts in Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+// @ts-check
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,json}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
