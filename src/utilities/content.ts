5d674fd533d6007ac4f89176a8dd871e3a490d55
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-06 21:05:31 +0800

    Create date-based post URL paths

--- src/utilities/content.ts
+++ src/utilities/content.ts
@@ -3,6 +3,7 @@ import type { CollectionEntry } from 'astro:content';
 type BlogPost = CollectionEntry<'blog'>;
 
 export const getPostUrl = (post: BlogPost) => {
-  // TODO: Use /year/month/slug format
-  return post.slug;
+  const year = post.data.date.getFullYear().toString();
+  const month = String(post.data.date.getMonth() + 1).padStart(2, '0');
+  return `/blog/${year}/${month}/${post.slug}`;
 };
