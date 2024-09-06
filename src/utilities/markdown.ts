d27ad8d652d7d58f4450c6202cc7aa109eef2872
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-06 19:43:50 +0800

    Create excerpt extractor pipeline

--- src/utilities/markdown.ts
+++ src/utilities/markdown.ts
@@ -1,9 +1,29 @@
+import type { Root } from 'mdast';
 import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
+import remarkStringify from 'remark-stringify';
 import { unified } from 'unified';
+import { visit } from 'unist-util-visit';
 
 export const mdToHtmlString = unified()
   .use(remarkParse)
   .use(remarkRehype)
   .use(rehypeStringify);
+
+function getFirstParagraph() {
+  return function transformer(tree: Root) {
+    visit(tree, 'root', (node) => {
+      node.children = node.children.filter(
+        (child) => child.type === 'paragraph'
+      );
+      node.children.length =
+        node.children.length > 1 ? 1 : node.children.length;
+    });
+  };
+}
+
+export const mdToExcerpt = unified()
+  .use(remarkParse)
+  .use(getFirstParagraph)
+  .use(remarkStringify);
