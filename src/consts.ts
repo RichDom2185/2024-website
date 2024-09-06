0a2ec7b6d8cd12c76108b6a38e0f9cfeb64d5714
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-06 18:11:10 +0800

    Update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "Richard's Blog";
+export const SITE_TITLE = 'Richard Dominick';
 export const SITE_AUTHOR = 'Richard Dominick';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
