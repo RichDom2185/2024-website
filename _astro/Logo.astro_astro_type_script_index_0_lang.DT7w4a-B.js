aedeea2585920facf848f2ce3a83e44dba68213e
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-09-07 08:17:55 +0000

    deploy: 6c1e8de5309f2c7cc99e174c15c70a1270801ade

---
+++ _astro/Logo.astro_astro_type_script_index_0_lang.DT7w4a-B.js
@@ -0,0 +1,1 @@
+const e=document.getElementById("site-logo");e?.addEventListener("click",t=>{t.metaKey||t.ctrlKey||window.location.pathname==="/"&&(t.preventDefault(),window.scrollTo({top:0}))});
---
+++ _astro/hoisted.CFaOsTjZ.js
@@ -0,0 +1,1 @@
+import"https://code.iconify.design/iconify-icon/2.1.0/iconify-icon.min.js";import"./Logo.astro_astro_type_script_index_0_lang.DT7w4a-B.js";
