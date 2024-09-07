72376804bd3959bd45378e84f23317f97769364d
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-08 01:08:20 +0800

    Add more social enum values

--- src/utilities/types/main/social.ts
+++ src/utilities/types/main/social.ts
@@ -3,6 +3,11 @@ export enum Social {
   GITHUB = 'GitHub',
   GITLAB = 'Gitlab',
   EMAIL = 'Email',
+  WHATSAPP = 'WhatsApp',
+  TIKTOK = 'TikTok',
+  FACEBOOK = 'Facebook',
+  TWITTER = 'Twitter',
+  TELEGRAM = 'Telegram',
 }
 
 export const socialToIconMap = Object.freeze({
@@ -10,6 +15,11 @@ export const socialToIconMap = Object.freeze({
   [Social.GITHUB]: 'ri:github-line',
   [Social.GITLAB]: 'ri:gitlab-line',
   [Social.EMAIL]: 'ri:mail-line',
+  [Social.WHATSAPP]: 'ri:whatsapp-line',
+  [Social.TIKTOK]: 'ri:tiktok-fill',
+  [Social.FACEBOOK]: 'ri:facebook-circle-fill',
+  [Social.TWITTER]: 'ri:twitter-x-fill',
+  [Social.TELEGRAM]: 'ri:telegram-2-fill',
 }) satisfies { [key in Social]: string };
 
 export const socialToColorClassMap = Object.freeze({
@@ -17,4 +27,9 @@ export const socialToColorClassMap = Object.freeze({
   [Social.GITLAB]: 'hover:text-[#FC6D26]',
   [Social.LINKEDIN]: 'hover:text-[#0B66C2]',
   [Social.EMAIL]: 'hover:text-[#EA4336]',
+  [Social.WHATSAPP]: 'hover:text-[#24D366]',
+  [Social.TIKTOK]: 'hover:text-[#FF004F]',
+  [Social.FACEBOOK]: 'hover:text-[#1877F2]',
+  [Social.TWITTER]: 'hover:text-[#198CD8]',
+  [Social.TELEGRAM]: 'hover:text-[#2AABEE]',
 }) satisfies { [key in Social]: string };
