5d81f2eafe94db7099ac3195947d0c0b5482192e
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-07 20:48:04 +0800

    Update post layout
    * Create special site title for blog posts
    * Generate page title based on post title and blog title
    * Generate page description based on post excerpt
    * Replace HTML comment with JS comment to optimize compiled file

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 export const SITE_TITLE = 'Richard Dominick';
+export const SITE_BLOG_TITLE = "Richard's Blog";
 export const SITE_AUTHOR = 'Richard Dominick';
 export const SITE_DESCRIPTION = "Richard's Personal Portfolio and Blog Site.";
 export const SITE_BASE_URL = 'https://richarddominick.me';
