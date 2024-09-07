42b6f4f9487e8dcef6aa61fb0f6331442c85b8d7
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-07 15:47:53 +0800

    Restore Markdown `linkTarget` functionality
    * Create custom transformer to set link attributes
    * Set link target in render pipeline
    * Allow setting rel attributes in link options

--- src/utilities/markdown.ts
+++ src/utilities/markdown.ts
@@ -1,4 +1,5 @@
-import type { Root } from 'mdast';
+import type { Root as HastRoot } from 'hast';
+import type { Root as MdastRoot } from 'mdast';
 import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
@@ -6,13 +7,46 @@ import remarkStringify from 'remark-stringify';
 import { unified } from 'unified';
 import { visit } from 'unist-util-visit';
 
-export const mdToHtmlString = unified()
-  .use(remarkParse)
-  .use(remarkRehype)
-  .use(rehypeStringify);
+export type LinkOptions = {
+  target?: string;
+  noopener?: boolean;
+  noreferrer?: boolean;
+  nofollow?: boolean;
+};
+
+function setLinkAttrs(options: LinkOptions = {}) {
+  const { target, noopener, noreferrer, nofollow } = options;
+  const rel = [
+    noopener && 'noopener',
+    noreferrer && 'noreferrer',
+    nofollow && 'nofollow',
+  ]
+    .filter(Boolean)
+    .join(' ');
+  return function transformer(tree: HastRoot) {
+    visit(tree, 'element', (node) => {
+      if (node.tagName !== 'a') {
+        return;
+      }
+      if (target) {
+        node.properties.target = target;
+      }
+      if (rel) {
+        node.properties.rel = rel;
+      }
+    });
+  };
+}
+
+export const mdToHtmlString = (linkOptions: LinkOptions) =>
+  unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(setLinkAttrs, linkOptions)
+    .use(rehypeStringify);
 
 function getFirstParagraph() {
-  return function transformer(tree: Root) {
+  return function transformer(tree: MdastRoot) {
     visit(tree, 'root', (node) => {
       node.children = node.children.filter(
         (child) => child.type === 'paragraph'
