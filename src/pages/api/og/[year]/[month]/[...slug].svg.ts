ec4c1c0f9468b6eec9e2b35d5a42c68d14441c33
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-09 01:27:41 +0800

    Set up endpoint for OG image generation

---
+++ src/pages/api/og/[year]/[month]/[...slug].svg.ts
@@ -0,0 +1,38 @@
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+import satori from 'satori';
+import type { BlogPost } from 'src/utilities/content';
+import { createNodes, getFont } from 'src/utilities/og';
+
+const fontRegular = await getFont('Plus Jakarta Sans', 400);
+const fontMedium = await getFont('Plus Jakarta Sans', 500);
+const fontBold = await getFont('Plus Jakarta Sans', 700);
+
+export async function getStaticPaths() {
+  const posts = await getCollection('blog');
+  return posts.map((post) => ({
+    params: {
+      year: post.data.date.getFullYear().toString(),
+      month: String(post.data.date.getMonth() + 1).padStart(2, '0'),
+      slug: post.slug,
+    },
+    props: post,
+  }));
+}
+
+export const GET: APIRoute = async ({ props }) => {
+  const nodes = await createNodes(props as BlogPost);
+  const svg = await satori(nodes as any, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      { name: 'Plus Jakarta Sans', data: fontRegular!, weight: 400 },
+      { name: 'Plus Jakarta Sans', data: fontMedium!, weight: 500 },
+      { name: 'Plus Jakarta Sans', data: fontBold!, weight: 700 },
+    ],
+  });
+  return new Response(svg, {
+    status: 200,
+    headers: { 'Content-Type': 'image/svg' },
+  });
+};
