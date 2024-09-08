696897a9b04702c1f2ef82a552cf897c7a9afa67
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-09-09 01:46:47 +0800

    Add OG metadata for blog posts

--- src/utilities/content.ts
+++ src/utilities/content.ts
@@ -7,3 +7,10 @@ export const getPostUrl = (post: BlogPost) => {
   const month = String(post.data.date.getMonth() + 1).padStart(2, '0');
   return `/blog/${year}/${month}/${post.slug}`;
 };
+
+export const getPostOgImage = (post: BlogPost) => {
+  // TODO: If custom OG image is set, return that
+  const year = post.data.date.getFullYear().toString();
+  const month = String(post.data.date.getMonth() + 1).padStart(2, '0');
+  return `/api/og/${year}/${month}/${post.slug}.png`;
+};
