18c06e0070f2f4ace25fabeb66711ee736487c99
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-05-26 17:32:48 +0800

    Fix outdated domain in deployment

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@ export const SITE_TITLE = 'Richard Dominick';
 export const SITE_BLOG_TITLE = "Richard's Blog";
 export const SITE_AUTHOR = 'Richard Dominick';
 export const SITE_DESCRIPTION = "Richard's Personal Portfolio and Blog Site.";
-export const SITE_BASE_URL = 'https://richarddominick.me';
-export const SITE_CANONICAL_URL = 'https://richarddominick.me';
+export const SITE_BASE_URL = 'https://gh.richarddominick.me';
+export const SITE_CANONICAL_URL = 'https://gh.richarddominick.me';
